4c9df76d30c09c5ab2334321e20f38ff84d8a7cd
Gilbert Francois Duivesteijn <info@gilbertfrancois.com>
2023-05-22 00:02:02 +0200

    Changed to streaming method.

--- src/c/main.c
+++ src/c/main.c
@@ -1,19 +1,10 @@
-#include <stdio.h>
 #ifdef __EMSCRIPTEN__
-#include <emscripten.h>
+#include <emscripten/emscripten.h>
+#else
+#define EMSCRIPTEN_KEEPALIVE
 #endif
 
-#ifdef __EMSCRIPTEN__
 EMSCRIPTEN_KEEPALIVE
-#endif
 int add(int a, int b) {
     return a + b;
-}
-
-#ifdef __EMSCRIPTEN__
-EMSCRIPTEN_KEEPALIVE
-#endif
-int main() {
-    printf("Hello world\n");
-    return 0;
 }
